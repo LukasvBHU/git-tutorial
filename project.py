3e6b3c347a7fb62ae52f6dd28972577bcdf3174e
LukasvBHU <lukas.vanbekkum@student.hu.nl>
2025-11-10 10:24:51 +0100

    nvt

--- project.py
+++ project.py
@@ -1,3 +1,8 @@
+
+
+def check_antwoord(antwoord):
+    return antwoord == 42
+
 def hello_world(lang):
     if lang == "NL":
         print("Hallo wereld!")
@@ -8,3 +13,8 @@ def hello_world(lang):
 
 hello_world("EN")
 
+antwoord = input("Wat is het antwoord op de ultieme vraag van het leven, het universum, en alles?")
+if check_antwoord(antwoord):
+    print("Correct!")
+else:
+    print("Mogelijk heb je een andere vraag in je hoofd?")
